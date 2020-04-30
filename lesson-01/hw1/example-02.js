225d7dbcc06a80a1464639ed7a96808e04dff082
Olena <elenapolis4uk@gmail.com>
2020-05-01 00:12:17 +0300

    removed readme info

---
+++ lesson-01/hw1/example-02.js
@@ -0,0 +1,8 @@
+const TOTAL = 100;
+const ORDERED = 50;
+
+if (ORDERED > TOTAL) {
+  console.log('На складе недостаточно твоаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
