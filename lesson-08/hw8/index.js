9fd9cb1a6f824b67f91d86123f5bf8b4dd8ed7fe
Olena <elenapolis4uk@gmail.com>
2020-06-02 14:07:14 +0300

    Minor fixes

--- lesson-08/hw8/index.js
+++ lesson-08/hw8/index.js
@@ -1,42 +1,47 @@
 import galleryItems from './gallery-items.js';
 
-const ul = document.querySelector('ul.js-gallery');
+const ulGallery = document.querySelector('ul.js-gallery');
 const divLightbox = document.querySelector('div.lightbox');
-const divContent = document.querySelector('div.lightbox__content');
-const img = document.querySelector('img.lightbox__image');
+const divLightboxContent = document.querySelector('div.lightbox__content');
+const divLightboxImg = document.querySelector('img.lightbox__image');
 const buttonClose = document.querySelector('button.lightbox__button');
-let clickedImg = '';
+let currentImg;
 
-galleryItems.forEach(obj => {
-  ul.innerHTML += `<li class="gallery__item">
+const imgHtml = galleryItems.reduce(
+  (acc, obj) =>
+    acc +
+    `<li class="gallery__item">
   <a class="gallery__link" href="${obj.original}">
     <img class="gallery__image" src="${obj.preview}" data-source="${obj.original}" alt="${obj.description}"/>
   </a>
-</li>`;
-});
+</li>`,
+  '',
+);
+
+ulGallery.insertAdjacentHTML('beforeend', imgHtml);
 
 function handleOpen(event) {
   event.preventDefault();
   const { target } = event;
 
   if (event.target === event.currentTarget) return;
-  clickedImg = event.target;
+  currentImg = event.target;
   const bigImgSrc = target.getAttribute('src');
   divLightbox.classList.add('is-open');
-  img.src = bigImgSrc;
+  divLightboxImg.src = bigImgSrc;
 }
 
 function handleClose(event) {
   event.preventDefault();
   divLightbox.classList.remove('is-open');
-  img.src = '';
+  divLightboxImg.src = '';
 }
 
-ul.addEventListener('click', handleOpen);
+ulGallery.addEventListener('click', handleOpen);
 
 buttonClose.addEventListener('click', handleClose);
 
-divContent.addEventListener('click', event => {
+divLightboxContent.addEventListener('click', event => {
   if (event.target === event.currentTarget) {
     handleClose(event);
   }
@@ -47,20 +52,28 @@ document.addEventListener('keydown', event => {
     if (event.keyCode === 27) {
       handleClose(event);
     } else {
-      const lis = [...ul.querySelectorAll('li')];
-      for (let i = 0; i < lis.length; i += 1) {
-        if (event.keyCode === 37 && lis[i].contains(clickedImg) && i !== 0) {
-          img.src = lis[i - 1].querySelector('a').getAttribute('href');
-          // clickedImg = lis[i - 1].querySelector('img');
+      const liNodes = [...ulGallery.querySelectorAll('li')];
+      for (let i = 0; i < liNodes.length; i += 1) {
+        if (
+          event.keyCode === 37 &&
+          liNodes[i].contains(currentImg) &&
+          i !== 0
+        ) {
+          divLightboxImg.src = liNodes[i - 1]
+            .querySelector('a')
+            .getAttribute('href');
+          currentImg = liNodes[i - 1].querySelector('img');
           break;
         }
         if (
           event.keyCode === 39 &&
-          lis[i].contains(clickedImg) &&
-          i !== lis.length - 1
+          liNodes[i].contains(currentImg) &&
+          i !== liNodes.length - 1
         ) {
-          img.src = lis[i + 1].querySelector('a').getAttribute('href');
-          clickedImg = lis[i + 1].querySelector('img');
+          divLightboxImg.src = liNodes[i + 1]
+            .querySelector('a')
+            .getAttribute('href');
+          currentImg = liNodes[i + 1].querySelector('img');
           break;
         }
       }
